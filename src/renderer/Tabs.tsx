6d0706639b6aaf5cd7962c39781e620a1c0ee963
Dane Bulat <dane.bulat@gmail.com>
2024-09-21 15:12:29 +0700

    setup draggable tabs with dnd-kit

--- src/renderer/Tabs.tsx
+++ src/renderer/Tabs.tsx
@@ -1,34 +1,106 @@
+import {
+	closestCenter,
+	DndContext,
+	DragEndEvent,
+	DragStartEvent,
+	KeyboardSensor,
+	PointerSensor,
+	useSensor,
+	useSensors } from "@dnd-kit/core";
+import {
+	restrictToHorizontalAxis,
+	restrictToParentElement
+} from "@dnd-kit/modifiers";
+import {
+	arrayMove,
+	horizontalListSortingStrategy,
+	SortableContext,
+	sortableKeyboardCoordinates,
+	useSortable
+} from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { useState } from "react";
 
-/* Tab Component */
+/* -------------------- */
+/* Tab Component        */
+/* -------------------- */
+
 interface TabProps {
 	id: number;
 	label: string;
 }
 
 const Tab: React.FC<TabProps> = ({ id, label }: TabProps) => {
-	console.log(id);
+	/* Dnd */
+	const {
+		attributes,
+	  listeners,
+		setNodeRef,
+		transform,
+		transition,
+	} = useSortable({ id, transition: { duration: 250, easing: 'cubic-bezier(0.25, 1, 0.5, 1)' }})
 
+	/* Styles */
 	const outerStyle: React.CSSProperties = {
+		transform: CSS.Transform.toString(transform),
+		transition,
 		backgroundColor: '#333',
 		padding: '0.25rem 1rem',
-		borderRadius: '0.5rem',
+		borderRadius: '0.25rem',
 		fontSize: '0.8rem',
 	};
 
 	return (
-		<div style={outerStyle}>
+		<div ref={setNodeRef} style={outerStyle} {...attributes} {...listeners}>
 			<span role="button">{label}</span>
 		</div>
 	);
 };
 
-/* Tabs Container */
+/* -------------------- */
+/* Tabs Container       */
+/* -------------------- */
+
 export const Tabs: React.FC = () => {
+	const [activeId, setActiveId] = useState<number | null>(null);
+	const [items, setItems] = useState<number[]>(
+		Array.from({ length: 5 }, (_, index) => index + 1)
+	);
+
+	/* Dnd */
+	const sensors = useSensors(
+		useSensor(PointerSensor),
+		useSensor(KeyboardSensor, {
+			coordinateGetter: sortableKeyboardCoordinates,
+		})
+	);
+
+	const handleDragStart = (event: DragStartEvent) => {
+		const { active } = event;
+		if (active) {
+			setActiveId(Number(active.id));
+		}
+	};
+
+	const handleDragEnd = (event: DragEndEvent) => {
+		const { active, over } = event;
+
+		if (over && active.id !== over.id) {
+			setItems((items) => {
+				const oldIndex = items.indexOf(Number(active.id));
+				const newIndex = items.indexOf(Number(over.id));
+				return arrayMove(items, oldIndex, newIndex);
+			})
+		}
+	};
+
+	/* Styles */
 	const outerStyle: React.CSSProperties = {
 		height: '48px',
 		backgroundColor: 'black',
 		borderBottom: '2px solid grey',
 	};
+
 	const innerStyle: React.CSSProperties = {
 		display: 'flex',
 		alignItems: 'center',
@@ -41,9 +113,17 @@ export const Tabs: React.FC = () => {
 	return (
 		<div style={outerStyle}>
 			<div style={innerStyle}>
-				<Tab id={1} label={'Tab 1'}/>
-				<Tab id={2} label={'Tab 2'}/>
-				<Tab id={3} label={'Tab 3'}/>
+				<DndContext
+					sensors={sensors}
+					collisionDetection={closestCenter}
+					onDragEnd={handleDragEnd}
+					onDragStart={handleDragStart}
+					modifiers={[restrictToHorizontalAxis, restrictToParentElement]}
+				>
+					<SortableContext items={items} strategy={horizontalListSortingStrategy}>
+						{items.map((id) => (<Tab key={String(id)} id={id} label={`Tab ${id}`}/> ))}
+					</SortableContext>
+				</DndContext>
 			</div>
 		</div>
 	);
